1a4b148270b49460cce93b3dad4b68b0201b265c
snufera <netostt91@gmail.com>
2023-05-28 23:22:04 -0300

    0.1 - Header done + making mobile menu

--- web/src/components/TopBar.vue
+++ web/src/components/TopBar.vue
@@ -1,5 +1,17 @@
 <script setup>
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+
+const navbar = ref('')
+
+function toggleNavbar() {
+  if (navbar.value === '') {
+    navbar.value = 'navbar--active'
+  } else {
+    navbar.value = ''
+  }
+}
 </script>
 
 <template>
@@ -9,10 +21,16 @@ import { RouterLink } from 'vue-router'
         <a href="#">Meu Pomodoro</a>
       </div>
 
-      <div class="navbar">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/pomodoro">Entenda mais</RouterLink>
-        <RouterLink to="/sobre">Quem somos</RouterLink>
+      <div class="mobilebar" @click="toggleNavbar">
+        <div class="line__1"></div>
+        <div class="line__2"></div>
+        <div class="line__3"></div>
+      </div>
+
+      <div class="navbar" :class="navbar">
+        <RouterLink to="/" class="navbar__routes">Home</RouterLink>
+        <RouterLink to="/pomodoro" class="navbar__routes">Entenda mais</RouterLink>
+        <RouterLink to="/sobre" class="navbar__routes">Quem somos</RouterLink>
       </div>
 
       <div class="user">
@@ -28,25 +46,111 @@ import { RouterLink } from 'vue-router'
 </template>
 
 <style lang="scss" scoped>
+@import '../assets/mixins';
+@import '../assets/variables';
+
 header {
   height: 80px;
   background-color: #222222;
+  border-bottom: 1px solid $txt-gray;
 
   nav {
     display: flex;
     align-items: center;
-    padding: 0 25px;
+    justify-content: space-between;
+    height: 100%;
+    padding: 0 30px;
 
     .logo {
-      position: absolute;
-      top: 30px;
+      @include mq(m) {
+        display: none;
+      }
 
       a {
         font-family: 'Tsukimi Rounded';
+        font-size: 20px;
         font-weight: 500;
-        text-decoration: none;
         text-transform: uppercase;
-        color: white;
+        @include textNone(white);
+      }
+    }
+
+    .navbar {
+      display: flex;
+      gap: 20px;
+
+      @include mq(m) {
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        gap: 0;
+        position: absolute;
+        top: 80px;
+        left: 0;
+        background-color: #333333;
+        width: 400px;
+        height: 500px;
+        transform: translateX(-100%)
+      }
+
+      &__routes {
+        @include textNone($txt-gray);
+
+        @include mq(m) {
+          color: white;
+          padding: 30px;
+
+          &:hover {
+            color: $txt-gray;
+          }
+        }
+      }
+    }
+
+    .navbar--active {
+      transform: translateX(0);
+    }
+
+    .user {
+      display: flex;
+      align-items: center;
+      gap: 20px;
+
+      &__login {
+        a {
+          @include txtUser(white);
+        }
+      }
+
+      &__signup {
+
+        a {
+          @include txtUser(rgb(141, 93, 38));
+          border-radius: 8px;
+          background-color: bisque;
+          padding: 10px;
+          transition: .35s;
+
+          &:hover {
+            background-color: rgb(192, 171, 146);
+          }
+        }
+      }
+    }
+
+    .mobilebar {
+      display: none;
+
+      @include mq(m) {
+        display: flex;
+        flex-direction: column;
+        gap: 6px;
+      }
+
+      div {
+        width: 26px;
+        height: 2px;
+        background-color: white;
       }
     }
   }
